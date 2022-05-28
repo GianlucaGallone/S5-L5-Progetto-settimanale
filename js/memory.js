5105fcb314dc4e3241f7d274295fcd779b235131
GianlucaGallone <cloud.gianluca@gmail.com>
2022-05-28 23:16:03 +0200

    aggiunto schermata Preview

--- js/memory.js
+++ js/memory.js
@@ -1,55 +1,16 @@
 let arrayAnimali = ['🐱', '🦉', '🐾', '🦁', '🦋', '🐛', '🐝', '🐬', '🦊', '🐨', '🐰', '🐯', '🐱', '🦉', '🐾', '🦁', '🦋', '🐛', '🐝', '🐬', '🦊', '🐨', '🐯', '🐰'];
-//libreria per icone
-//https://html-css-js.com/html/character-codes/
-
 let arrayComparison = [];
-document.body.onload = startGame(); // chiama la funzione al caricamento della pagina
+const start = document.querySelector('.text-center .start');
+const timer = document.querySelector('.text-center .timer');
+let interval;
+const modalFind = document.querySelector('#modal');
 
-var modalFind = document.querySelector('#modal');
+document.body.onload = gamePreview(); // al caricamento della pagina chiama la funzione e genera il contenuto
 
 //#region ---------------- Traccia (1) --------------------
 
 // mi serviranno alcune variabili 1. interval 2. una agganciata alla classe find 
 // 3. una agganciata al'id modal 4. una agganciata alla classe timer
-
-//#endregion
-
-//#region ------------- Implementazione 1) Countdown Partita ---------------------
-//@desc Chiamare una funzione ogni tot millisecondi che avra' un counter--; countdown entro il quale termina la partita
-
-let timer = document.querySelector('.text-center .timer');
-let sec = 10; // valori di partenza  
-let min = 0;
-printTimer();
-setInterval(countdown, 1000); // richiama la funzione ogni secondo
-
-function countdown() {
-
-    sec--;         // secondi decrescono al richiamo della funzione
-    if(sec <= 0) { // se i secondi sono inferiori/uguali a 0
-        min--;     // i minuti scendono di 1
-        sec = 59;  // i secondi salgono a 59
-    }
-    printTimer();
-    countdown_stop();
-}
-
-function countdown_stop() {
-
-    if(min <= -1) { 
-        clearInterval(countdown); // stop al countdown
-        printResult();            // stampa game over
-        min = 0;
-        sec = 0;
-    }
-}
-
-function printTimer() {
-    timer.innerHTML = 'Tempo: ' + +min + ' min ' + +sec + ' sec';
-}
-function printResult() {
-    timer.innerHTML = 'Game Over. Hai perso!';
-}
 
 //#endregion
 
@@ -64,20 +25,57 @@ function printResult() {
 
 //#endregion
 
-//#region ------------- Implementazione 2) Creazione dei div per Contenitore e Icone ---------------------
+function pressStart() { // Gioco e Timer collegati al pulsante Start
+    timerStop();  // 1) stop all'intervallo precedente
+    clearTimer(); // pulizia del div
+    
+    gameInit();   // caricamento contenuto del gioco
+    timerStart(); // 2) caricamento nuovo intervallo
 
-/* function restart() {
+}
 
-    startGame();
-} */
+function pressRestart() {
+    stop();
+    timerStop();  // stop all'intervallo precedente
+    clearTimer(); // pulizia del div
+    
+    gameInit();   // caricamento contenuto del gioco
+    timerStart(); // caricamento nuovo intervallo
+}
 
+/* -------------------------------------------------------------------------------------------------------------------------- */
 
+function gamePreview() { // Schermata Preview del gioco senza input
+    let startButton = document.createElement('input'); // Creazione elemento
+    startButton.type = 'button';
+    startButton.id = 'button';
+    startButton.className = 'button';
+    startButton.value = 'Start';                       // Testo da inserire
+    startButton.onclick = function() {pressStart();}   // Assegnare funzione ad evento onclick
+    start.appendChild(startButton);                    // Scriverlo nel nodo                                
+    
+    var iconsFind = document.getElementById('griglia');       
+    timer.innerHTML = 'Memory Game: 5 minutes to win!'; 
+    iconsFind.innerHTML = '';                                 
+    for(i=0; i<24; i++) {
+        let divCont = document.createElement('div');          
+        let divIcon = document.createElement('div');          
+        divIcon.className = 'icon';                           
+        iconsFind.appendChild(divCont).appendChild(divIcon);                    
+    }
+}
 
-function startGame() {
+function gameInit() {    // Ciclo creazione 2 div cont ed icone, e evento onclick
+
+    // Per selezionare e modificare un button esistente
+    let restartButton = document.querySelector('.text-center .start input#button');
+    restartButton.value = 'Restart';
+    restartButton.onclick = function() {pressRestart();}
+
     var arrayShuffle = shuffle(arrayAnimali);                 // gli passo la funzione shuffle con all'interno le icone                
     var iconsFind = document.getElementById('griglia');       // assegno alla var cards il div html (non il contenuto)
-    iconsFind.innerHTML = '';                                 // pulisco tutto il contenuto
     
+    iconsFind.innerHTML = '';                                 // pulisco tutto il contenuto al riavvio del browser/button ricomincia
     for(i=0; i<24; i++) {
         let divCont = document.createElement('div');          // div contenitore
         let divIcon = document.createElement('div');          // div con icona dentro
@@ -86,7 +84,7 @@ function startGame() {
         iconsFind.appendChild(divCont).appendChild(divIcon);  // assegno il div alla destinazione e assegno le icone
         divIcon.innerHTML = arrayShuffle[i];                  // assegno shuffle(icone) nel div delle icone
         
-/*         let iconClass = document.getElementsByClassName('icon');
+/*         let iconClass = document.querySelector('#griglia>div');
         iconClass.addEventListener('click', function(){
 
             shuffle(a);
@@ -96,7 +94,50 @@ function startGame() {
     }
 }
 
-//#endregion
+let sec = 10; // valori di partenza  
+let min = 0;
+
+//poi al click del pulsante start chiama setInterval con funzione:
+function timerInit() {   // Timer countdown della partita
+
+    sec--;         // i secondi decrescono al richiamo della funzione
+    if(sec <= 0) { // se i secondi sono inferiori/uguali a 0
+        min--;     // i minuti scendono di 1
+        sec = 59;  // i secondi salgono a 59
+    }
+/*     if(min <= -1) { 
+        timerStop();   // ferma intervallo     
+        min = 0;
+        sec = 10;
+        printResult(); // stampa game over
+    }  */
+
+    printTimer();  // stampi il timer
+    console.log(timer);
+}
+
+function timerStart() {
+    interval = setInterval(timerInit, 1000);
+}
+
+function timerStop() {
+    
+    clearInterval(interval);
+}
+
+function printTimer() {
+    timer.innerHTML = 'Tempo: ' + +min + ' min ' + +sec + ' sec';
+}
+function printResult() {
+    timer.innerHTML = 'Game Over. Hai perso!';
+}
+
+function clearTimer() {  // Cancella il testo nel div
+
+    timer.innerHTML = '';
+}
+
+/* -------------------------------------------------------------------------------------------------------------------------- */
 
 function shuffle(a) {
     var currentIndex = a.length;
